dcc07922a647245c60a9d66f636c9aaaf0ae78a7
Khang Phan <khang.phan@opswat.com>
2021-07-05 18:44:28 +0700

    Add checkbox, mixin, styles button - adjust Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import FocusTrap from "focus-trap-react";
+
 import classes from './Modal.module.scss'
 import Backdrop from 'components/UI/Backdrop/Backdrop'
 import PropTypes from 'prop-types'
+
 
 const modal = (props) => {
   const {
@@ -10,21 +13,29 @@ const modal = (props) => {
     height,
     show,
     modalClosed,
+    className : classname
   } = props
-  return <>
-    <Backdrop show={show} clicked={modalClosed} />
-    <div className={classes.Modal} style={{
-      transform: show ? "translate(-50%, -50%)" : "translate(-50%, -200%)",
-      left: show ? '50%' : '50%',
-      top: show ? '50%' : '50%',
-      opacity: show ? '1' : '0',
-      zIndex: show ? '9999' : '-1',
-      width: width,
-      height: height,
-    }}>
-      {props.children}
+  return <FocusTrap
+    focusTrapOptions={{
+      initialFocus: props.autoFocus ? this.props.autoFocus : false,
+      clickOutsideDeactivates: true,
+    }}
+  >
+    <div className={classes.ModalFrame}>
+      <Backdrop show={show} clicked={modalClosed} />
+      <div className={[classes.Modal, classname].join(" ")} style={{
+        transform: show ? "translate(-50%, -50%)" : "translate(-50%, -200%)",
+        left: show ? '50%' : '50%',
+        top: show ? '50%' : '50%',
+        opacity: show ? '1' : '0',
+        zIndex: show ? '9999' : '-1',
+        width: width,
+        height: height,
+      }}>
+        {props.children}
+      </div>
     </div>
-  </>
+  </FocusTrap>
 }
 
 
@@ -32,13 +43,15 @@ modal.propTypes = {
   width: PropTypes.string,
   height: PropTypes.string,
   show: PropTypes.bool,
-  modalClosed: PropTypes.func
+  modalClosed: PropTypes.func,
+  classname : PropTypes.string
 }
 
 modal.defaultProps = {
   width: '35rem',
-  height: '15.5rem',
+  height: '20rem',
   show: false,
+  classname : ""
 }
 
 export default modal
--- src/containers/HomeBuilder/HomeBuilder.js
+++ src/containers/HomeBuilder/HomeBuilder.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
-// Redux
+// redux
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { homeActions } from "redux/actions";
@@ -8,21 +7,40 @@ import { createStructuredSelector } from "reselect";
 import {
 
 } from "redux/selectors";
+// style
 import classes from './HomeBuilder.module.scss';
 // components
 import Timeline from 'components/Timeline/Timeline';
+import Modal from "components/UI/Modal/Modal"
 class HomeBuilder extends Component {
 
+  state = {
+    confirmPopup: true
+  }
+
+  toggleConfirmHandler = (active) => {
+    this.setState({ confirmPopup: active })
+  }
 
   componentDidMount() {
     this.props.getHomeInfomation();
   }
 
   render() {
+    const { confirmPopup } = this.state;
     return (
       <div className={classes.Background}>
         <h2>Welcome to Animation Boilerplate</h2>
-        <Timeline/>
+        <Timeline />
+        <Modal
+          modalClosed={() => this.toggleConfirmHandler(false)}
+          show={confirmPopup}
+          className={classes.Popup}
+        >
+          Cookie Accept
+          <br/>
+          <button onClick={() => this.toggleConfirmHandler(false)}>OK</button>
+        </Modal>
       </div>
     )
   }
